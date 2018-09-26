4aff2ea60d89cce37423fbf88b5264a72bdce46c
Maayan Kfir <mayankfir@gmail.com>
2018-09-26 16:10:46 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,9 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  the_manifest = {"whale bone corsets" => 5,
+     "porcelain vases" => 2,
+      "oil paintings" => 3 }
 end
 
 def retrieval
@@ -24,13 +26,13 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-
+shipping_manifest["muskets"] = 2
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
-
+shipping_manifest
 end
